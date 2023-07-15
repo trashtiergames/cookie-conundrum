448b26d3350d8fde573a50854c358ad476b43bec
trashtiergames <hello@trashtiergames.com>
2023-07-15 22:14:51 +0300

    Add move menu state boilerplate

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -8,7 +8,8 @@ require "src/Button"
 
 require "src/StateStack"
 require "src/StateMachine"
-require "src/states/BaseState"
 
+require "src/states/BaseState"
 require "src/states/TitleState"
 require "src/states/PlayState"
+require "src/states/MoveMenuState"
---
+++ src/states/MoveMenuState.lua
@@ -0,0 +1,21 @@
+-- Display menu for moving between locations
+
+MoveMenuState = Class{__includes = BaseState}
+
+function MoveMenuState:init()
+  
+end
+
+function MoveMenuState:enter()
+end
+
+function MoveMenuState:exit()
+end
+
+function MoveMenuState:update()
+  
+end
+
+function MoveMenuState:render()
+  
+end
